c4197e889b7816654d0a4c06f15380d7cbb5309b
jdelapart <jules.delapart@etudiant.univ-rennes1.fr>
2023-12-28 17:03:48 +0100

    Initial commit for new repo AOC

--- 2015/J4.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-IN = "yzbqklnj"
-hashihasha = hashlib.md5(IN.encode()).hexdigest()
-print(hashihasha[0:5])
-i=0
-while hashihasha[0:6] != "000000" :
-    hashihasha = hashlib.md5((IN + str(i)).encode()).hexdigest()
-    i+=1
-print(hashihasha,i-1)
